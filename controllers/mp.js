4e2a0d3d812b97aae5af56da97c671c6f1f14e56
Fernando Olivero <fernando.olivero@ecloudsolutions.com>
2021-06-23 18:05:44 -0300

    FIX NOTIFICATION URL MP

--- controllers/mp.js
+++ controllers/mp.js
@@ -1,11 +1,27 @@
-var paymentPayer = require("../catalogs/payment-payer.json");
 var paymentService = require("../services/payment")
-var paymentHelper = require("../helpers/payment")
 
 module.exports = async function(req, res) {
     try {
         console.log(`WEB HOOK: BODY ${JSON.stringify(req.body)} QUERY ${JSON.stringify(req.query)}`)
+        let query = req.query;
+        let body = req.body;
+        if ((query.type && query['data.id']) || (query.topic && query.id)) {
+            const id = query['data.id'] ? query['data.id'] : query.id;
+            const type = query.type ? query.type : query.topic;
+            switch (type) {
+                case "payment":
+                    const response = await paymentService.handleIpnPaymentRequest(id)
+                    return res.sendStatus(response)
+                    break;
+                default:
+                    console.error(`webhook: Undefined notification type ${type} BODY ${JSON.stringify(body)} QUERY ${JSON.stringify(query)}`);
+                    throw new Error("Error recibiendo la respuesta de Mercado Pago");
+            }
+        } else {
+            return res.sendStatus(200);
+        }
     } catch (err) {
-        throw err
+        console.error(err);
+        return res.sendStatus(500);
     }
 }
